fcf3a1cae64fd172fec4298d6a6beea4f8c46538
趙子賢 <danny900714@gmail.com>
2021-08-16 22:26:02 +0800

    Install and configure Material-UI

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,21 @@
+import ThemeProvider from '@material-ui/styles/ThemeProvider';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import routes from './constant/routes.json';
+import defaultTheme from './theme';
 import Login from './views/Login/Login';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path={routes.LOGIN} component={Login} />
+        <ThemeProvider theme={defaultTheme}>
+            <BrowserRouter>
                 <Switch>
+                    <Route exact path={routes.LOGIN} component={Login} />
+                    <Switch>
 
+                    </Switch>
                 </Switch>
-            </Switch>
-        </BrowserRouter>
+            </BrowserRouter>
+        </ThemeProvider>
     );
 }
 
---
+++ web/src/theme.ts
@@ -0,0 +1,20 @@
+import { pink } from "@material-ui/core/colors";
+import { createTheme } from "@material-ui/core/styles";
+
+const defaultTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#4F46E5',
+        },
+        secondary: pink
+    },
+    typography: {
+        fontFamily: [
+            'Noto Sans TC',
+            'sans-serif',
+        ].join(','),
+    },
+    spacing: factor => `${0.25 * factor}rem`,
+});
+
+export default defaultTheme;
