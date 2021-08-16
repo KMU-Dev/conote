ff506cea798dbfbaeab848359d6cbad7ca04a865
趙子賢 <danny900714@gmail.com>
2021-08-16 21:37:50 +0800

    Remove Tailwind CSS and add React Router DOM

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,27 +1,18 @@
-import React from 'react';
-import './index.css';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import routes from './constant/routes.json';
+import Login from './views/Login/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path={routes.LOGIN} component={Login} />
+                <Switch>
+
+                </Switch>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
