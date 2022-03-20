c4cb4ff90b2d509381aac7c3b11022934a595aca
趙子賢 <danny900714@gmail.com>
2022-03-20 18:18:49 +0800

    Finish Http and express Sentry tracing

--- api/src/sentry/sentry.interfaces.ts
+++ api/src/sentry/sentry.interfaces.ts
@@ -1,12 +1,7 @@
 import { Handlers, NodeOptions } from '@sentry/node';
 
-export interface SentryModuleTracingOptions {
-    enabled: boolean;
-}
-
 export interface SentryModuleOptions {
     enabled?: boolean;
     init?: NodeOptions;
     requestHandler?: Handlers.RequestHandlerOptions;
-    tracing?: SentryModuleTracingOptions;
 }
