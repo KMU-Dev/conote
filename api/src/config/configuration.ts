dbfaddcdc15d61c8907b5fa6610ff03e587cde79
趙子賢 <danny900714@gmail.com>
2022-03-22 23:09:44 +0800

    Support deep merge in api configuration

--- api/src/config/configuration.ts
+++ api/src/config/configuration.ts
@@ -1,13 +1,14 @@
 import { resolve } from 'app-root-path';
-import { plainToClass } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
 import { validateSync } from 'class-validator';
 import { readFileSync } from 'fs';
 import * as glob from 'glob';
 import * as yaml from 'js-yaml';
+import merge from 'lodash/merge';
 import { InvalidConfigError } from './config.error';
 import { AppConfig } from './schema/app.schema';
 
-const defaultConfig = resolve('config/default.yaml');
+const defaultConfigPath = resolve('config/default.yaml');
 
 export default () => {
     const filenames = glob.sync('config/*.yaml');
@@ -15,17 +16,17 @@ export default () => {
     const configs: unknown[] = [];
 
     // load default config
-    configs.push(yaml.load(readFileSync(defaultConfig, 'utf-8')));
+    const defaultConfig = yaml.load(readFileSync(defaultConfigPath, 'utf-8'));
 
     for (const filename of filenames) {
         const filepath = resolve(filename);
         configs.push(yaml.load(readFileSync(filepath, 'utf-8')));
     }
 
-    const config = Object.assign({}, ...configs);
+    const config = merge(defaultConfig, ...configs);
 
     // validate config
-    const validatedConfig = plainToClass(AppConfig, config);
+    const validatedConfig = plainToInstance(AppConfig, config);
     const errors = validateSync(validatedConfig);
     if (errors.length > 0) throw new InvalidConfigError(errors);
 
