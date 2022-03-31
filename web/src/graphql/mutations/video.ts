ec805f1a9d00212593955f8988ea26ab7dbdae96
趙子賢 <danny900714@gmail.com>
2022-03-31 23:20:20 +0800

    Add video upload custom event

--- web/src/graphql/mutations/video.ts
+++ web/src/graphql/mutations/video.ts
@@ -4,6 +4,7 @@ export const UPLOAD_VIDEO = gql`
     mutation UploadVideo($vodcfsInput: UploadVodcfsVideoInput!) {
         uploadVideo(vodcfsInput: $vodcfsInput) {
             id
+            title
             vodcfsVideo {
                 id
                 status
