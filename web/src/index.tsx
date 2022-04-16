8c22e1d126847ccfa7c172599c01bd25681f06a1
趙子賢 <danny900714@gmail.com>
2022-04-16 23:27:49 +0800

    Use createRoot to opt in react v18

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import 'reflect-metadata';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Root from './Root';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
         <Root />
     </React.StrictMode>,
-    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
