491ed8929ca85611db846b60b65dd38615b2295c
趙子賢 <danny900714@gmail.com>
2022-05-21 19:53:06 +0800

    Upgrade @apollo/client from 3.5.10 to 3.6.4 and add back StrictMode (#112)

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -7,13 +7,11 @@ import Root from './Root';
 
 const root = createRoot(document.getElementById('root'));
 
-// Opt out StrictMode since @apollo/client doesn't support react v18
-/* root.render(
+root.render(
     <React.StrictMode>
         <Root />
     </React.StrictMode>,
-); */
-root.render(<Root />);
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
